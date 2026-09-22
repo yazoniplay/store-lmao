24d83fa221b6f9f6756e31d9eb544d9e9f6fe2aa
yazoniplay <yazan.mahmoud2@outlook.com>
2026-09-22 22:03:08 +0200

    Connect Velora frontend to Supabase

--- velora-config.js
+++ velora-config.js
@@ -1,6 +1,6 @@
 window.VELORA_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://klzdnzcegyyelzgsxfdw.supabase.co",
+  supabaseAnonKey: "sb_publishable_2JNQv0-lYSIyFX4P5EhBjg_Zuu0F1_Y",
   checkoutFunction: "create-checkout",
   currency: "eur"
 };
